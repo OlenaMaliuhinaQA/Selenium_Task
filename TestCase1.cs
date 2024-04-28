680e1817da27ac99beb3ff6651d7c95d7c370d4b
Olena Maliuhina <olena_maliuhina@epam.com>
2024-04-28 21:53:05 +0200

    Initial commit

---
+++ TestCase1.cs
@@ -0,0 +1,78 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumProject1
+{
+    public class TestCase1
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(@"https://www.epam.com/");
+
+            driver.Manage().Window.Maximize();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            // driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);          
+           
+            driver.FindElement(By.Id("onetrust-accept-btn-handler")).Click();
+            driver.FindElement(By.XPath("//a[text()='Careers']")).Click();
+            driver.FindElement(By.XPath("//*[@id='new_form_job_search-keyword']")).SendKeys("Java");
+            driver.FindElement(By.XPath("//span[@class='select2-selection__arrow']")).Click();
+            driver.FindElement(By.XPath("//li[@title='All Cities in Spain']")).Click();
+            driver.FindElement(By.XPath("//label[contains(@class, 'recruiting-search__filter-label-23') and contains(@class, 'checkbox-custom-label') and contains(@class, 'body-text-small')]")).Click();
+            driver.FindElement(By.XPath("//button[contains(@class, 'small-button-text small-button-transparent-preset center-background-preset small-round-gradient-border job-search-button-transparent-23')]")).Click();
+            driver.FindElement(By.XPath("//ul[@class=\"search-result__list\"]/li[last()]")).Click();
+
+            
+            // Find all search result items
+            var searchResultItems = driver.FindElements(By.XPath("//ul[@class='search-result__list']/li[contains(@class, 'search-result__item')]"));
+
+            // Find the last search result item
+            IWebElement lastSearchResultItem = searchResultItems[searchResultItems.Count - 1];
+
+            // Find the "View and apply" button within the last search result item
+            IWebElement viewAndApplyButton = lastSearchResultItem.FindElement(By.XPath(".//a[contains(@class, 'search-result__item-apply-23')]"));
+
+            // Click on the "View and apply" button
+            viewAndApplyButton.Click();
+
+           IWebElement vacancyContent = driver.FindElement(By.XPath("//div[@class='vacancy-details-23__vacancy-content vacancy_content']"));
+          bool containsJava = vacancyContent.Text.Contains("Java");
+          Assert.Pass("The vacancy content contains 'Java'.");
+
+            driver.Quit();
+
+            //IWebElement accept_cookies = driver.FindElement(By.Id("onetrust-accept-btn-handler"));
+            //accept_cookies.Click();
+
+            //IWebElement link = driver.FindElement(By.XPath("//a[text()='Careers']"));
+            //link.Click();
+
+            //IWebElement carriersearch = driver.FindElement(By.XPath("//*[@id='new_form_job_search-keyword']"));
+            //carriersearch.SendKeys("Java");
+            //carriersearch.SendKeys(Keys.Return);
+
+            //IWebElement all_cities_dropdown = driver.FindElement(By.XPath("//span[@class='select2-selection__arrow']"));
+            //all_cities_dropdown.Click();
+
+            //IWebElement all_cities_in_spain_option = driver.FindElement(By.XPath("//li[@title='All Cities in Spain']"));
+            //all_cities_in_spain_option.Click();
+
+            //IWebElement select_remote = driver.FindElement(By.XPath("//label[contains(@class, 'recruiting-search__filter-label-23') and contains(@class, 'checkbox-custom-label') and contains(@class, 'body-text-small')]"));
+            //select_remote.Click();
+
+            //IWebElement find_button = driver.FindElement(By.XPath("//button[contains(@class, 'small-button-text small-button-transparent-preset center-background-preset small-round-gradient-border job-search-button-transparent-23')]"));
+            //find_button.Click();    
+
+        }
+    }
+}
---
+++ TestCase2.cs
@@ -0,0 +1,65 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace SeleniumProject1
+{
+    public class Tests2
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test2()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(@"https://www.epam.com/");
+
+            driver.Manage().Window.Maximize();
+
+
+            //2.	Find a magnifier icon and click on it
+            driver.FindElement(By.XPath("//span[contains(@class, 'search-icon') and contains(@class, 'dark-iconheader-search__search-icon')]")).Click();
+
+            //3.	Find a search string and put there “BLOCKCHAIN”/”Cloud”/”Automation” (use as a parameter for a test)
+                    
+            IWebElement inputElement = driver.FindElement(By.Id("new_form_search"));
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+            wait.Until(d => inputElement.Displayed);
+            inputElement.SendKeys("BLOCKCHAIN/Cloud/Automation");
+
+            //4.Click “Find” button
+            driver.FindElement(By.XPath("//span[contains(text(), 'Find')]")).Click();
+           // driver.FindElement(By.CssSelector("span.find")).Click();
+
+
+            string htmlContent = "<div class=\"search-results__items\">...</div>";
+
+            // Load the HTML content into an XElement
+            XElement root = XElement.Parse(htmlContent);
+
+            // Query to check if all <article> elements contain the specified words
+            bool allContainWords = root.XPathSelectElements("//article")
+                .All(article =>
+                    article.Value.Contains("BLOCKCHAIN", StringComparison.OrdinalIgnoreCase) ||
+                    article.Value.Contains("Cloud", StringComparison.OrdinalIgnoreCase) ||
+                    article.Value.Contains("Automation", StringComparison.OrdinalIgnoreCase));
+
+            // Assert if all <article> elements contain the specified words
+            if (allContainWords)
+            {
+                Assert.Pass("All <article> elements contain the specified words.");
+            }
+            else
+            {
+                Assert.Fail("Not all <article> elements contain the specified words.");
+            }
+          driver.Quit();  
+        }
+    }
+}
